3a617df207e5cce80c40fa1297d95efc7e2b299b
Giacomo Ritucci <giacomo.ritucci@email.it>
2012-02-20 00:27:58 +0100

    List of recipes

--- practicals/cookbook-app/public/js/cookbook.js
+++ practicals/cookbook-app/public/js/cookbook.js
@@ -1,10 +1,12 @@
-var COOKBOOK = {}
+/*
+ * TODO:
+ *
+ * login
+ * new recipe author must be the logged in user
+ */
 
-COOKBOOK.models = {};
 
-COOKBOOK.models.Recipe = Backbone.Model.extend({
-
-    urlRoot: "/recipes",
+Recipe = Backbone.Model.extend({
 
     validate: function (attrs) {
         if (!attrs.author) {
@@ -17,4 +19,78 @@ COOKBOOK.models.Recipe = Backbone.Model.extend({
             return "Instructions not set";
         }
     }
+});
+
+
+Recipes = Backbone.Collection.extend({
+
+    model: Recipe,
+
+    url: '/recipes'
+});
+
+
+RecipeListView = Backbone.View.extend({
+
+    tagName: "ul",
+
+    id: "recipes",
+
+    initialize: function () {
+        _.bindAll(this, 'addItem', 'render');
+    },
+
+    addItem: function (model) {
+        var itemView = new RecipeListItemView({model: model});
+        itemView.render();
+        $(this.el).append(itemView.el);
+    },
+
+    render: function () {
+        this.collection.each(this.addItem);
+    }
+});
+
+RecipeListItemView = Backbone.View.extend({
+
+    tagName: "li",
+
+    className: "clickable",
+
+    initialize: function () {
+        _.bindAll(this, "render");
+    },
+
+    events: {
+        "click": "open"
+    },
+
+    render: function () {
+        var template = $("#recipe-list-view-template").html();
+        $(this.el).html(_.template(template, this.model.toJSON()));
+    },
+
+    open: function () {
+        console.log(this.model.toJSON());
+    }
+});
+
+
+$(function () {
+    var recipes = new Recipes(),
+        list = new RecipeListView({
+            collection: recipes
+        });
+
+    window.recipes = recipes;
+
+    list.render();
+    $("#content").append(list.el);
+
+    recipes.on("add", list.addItem);
+
+    recipes.fetch({
+        success: function (coll, res) { list.render(); },
+        error: function (coll, res) { alert("Errore!"); }
+    });
 });
